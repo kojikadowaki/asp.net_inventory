a515c3a57e00cceb42a5de0974ea5e0d8c04061b
kadowaki <gbsg9405@yahoo.co.jp>
2024-03-07 14:04:06 +0900

    Initial commit

---
+++ HomeInventory/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' アセンブリに関する一般情報は、以下の属性セットによって 
+' 制御されます。アセンブリに関連付けられている情報を変更するには、
+' これらの属性値を変更します。
+
+' アセンブリの属性値を確認します
+<Assembly: AssemblyTitle("HomeInventory")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("HomeInventory")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'このプロジェクトが COM に公開される場合、次の GUID がタイプ ライブラリの ID になります。
+<Assembly: Guid("3e788e2e-aebc-4625-a319-3c0d25fcc9e7")>
+
+' アセンブリのバージョン情報は、以下の 4 つの値で構成されています:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
+' 既定値にすることができます:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
